7dd5f656af3687b8a43e70372e8337e8320fec03
Matthijs-de-Graaf <117952288+Matthijs-de-Graaf@users.noreply.github.com>
2023-03-22 11:27:13 +0100

    Update Database.sql
    This is better

--- Database.sql
+++ Database.sql
@@ -1,28 +1,24 @@
--- Create the database
 DROP DATABASE IF EXISTS ALA_Questions_test;
 CREATE DATABASE ALA_Questions_test;
 
 -- Select the database
 USE ALA_Questions_test;
 
--- Create the questions table
+
 CREATE TABLE questions (
-  ID INT NOT NULL AUTO_INCREMENT,
-  question VARCHAR(255),
-  option_a BOOLEAN,
-  option_b BOOLEAN,
-  next_question_a BOOLEAN,
-  next_question_b BOOLEAN,
-  PRIMARY KEY (ID)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    question varchar(255),
+    score INT NOT NULL
 );
 
--- Insert data into the table
-INSERT INTO ALA_Questions_test.questions (question, option_a, option_b, next_question_a, next_question_b) VALUES 
-    ('True', TRUE, TRUE, TRUE, TRUE),
-    ('this is false', FALSE, FALSE, FALSE, FALSE),
-    ('Ehhh -Renzo (true)', FALSE, FALSE, FALSE, FALSE),
-    ('Anil is HOMOPHOBIC', FALSE, FALSE, FALSE, FALSE),
-    ('Matthijs is stupid and gay', FALSE, FALSE, FALSE, FALSE);
+INSERT INTO questions (question, score) VALUES 
+('Why wouldnt a simple change in name not work?', 3),
+('Why wouldnt a simple change in name not work?', 2),
+('Why wouldnt a simple change in name not work?', 1),
+('Why wouldnt a simple change in name not work?', -1),
+('Why wouldnt a simple change in name not work?', -3),
+('Why wouldnt a simple change in name not work?', -2);
+
 
 -- Query the table to get information about the responses
 SELECT *
